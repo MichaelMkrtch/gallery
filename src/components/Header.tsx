febdf2f085632dfc93f4e31b41cfa5fe991ab8c0
Michael Mkrtchyan <michaelmkrtch.dev@gmail.com>
2025-05-21 13:09:35 -0700

    feat: add basic header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,59 @@
+"use client";
+
+import { Input } from "./ui/input";
+import { Search } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Header() {
-  return <header>Gallery</header>;
+  return (
+    <header className="border-foreground/5 relative mx-10 flex items-center justify-between rounded-br-3xl rounded-bl-3xl border border-t-0 px-10 py-6 shadow-xs">
+      <Link
+        href="/"
+        className="hover:text-primary flex w-[270px] items-center justify-start gap-8"
+      >
+        <Image
+          src="/logo-simple-dark.png"
+          alt="Agajanian Gallery logo (AG)."
+          height="44"
+          width="44"
+        />
+        <div className="bg-foreground absolute top-11 left-20 h-0.5 w-10 rotate-90 rounded-tl-sm rounded-br-sm" />
+        <h1 className="text-primary flex flex-col text-lg leading-5 font-semibold tracking-tight transition-colors duration-150 ease-in">
+          <span>AGAJANIAN</span>
+          <span>GALLERY</span>
+        </h1>
+      </Link>
+
+      <nav>
+        {/* <ul> used for accessibility. Screen readers notify about the presence
+          of a list and the number of list items. */}
+        <ul className="flex justify-around gap-3.5 text-lg font-medium tracking-tight">
+          <li className="navlink">
+            <Link href="/artists">Artists</Link>
+          </li>
+          <li className="navlink">
+            <Link href="/collections">Collections</Link>
+          </li>
+          <li className="navlink">
+            <Link href="/artworks">Artworks</Link>
+          </li>
+          <li className="navlink">
+            <Link href="/about">About</Link>
+          </li>
+          <li className="navlink">
+            <Link href="/contact">Contact Us</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <div className="border-foreground/10 relative flex items-center justify-center rounded-full border-1 shadow-xs">
+        <Search size="20" className="ml-2" />
+        <Input
+          className="placeholder:text-foreground/60 h-9 w-60 border-none font-semibold shadow-none placeholder:font-medium focus-visible:ring-0"
+          placeholder="Search"
+        />
+      </div>
+    </header>
+  );
 }
